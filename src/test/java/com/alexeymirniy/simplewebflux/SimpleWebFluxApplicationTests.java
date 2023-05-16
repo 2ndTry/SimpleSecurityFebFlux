e7ff580160bab02ab1f37e96fbe971e70e046f6a
2ndTry <mirnijalex@gmail.com>
2023-05-16 15:09:17 +0300

    Start project

---
+++ src/test/java/com/alexeymirniy/simplewebflux/SimpleWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexeymirniy.simplewebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
